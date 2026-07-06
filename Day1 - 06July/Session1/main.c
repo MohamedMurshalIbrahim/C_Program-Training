fe3e1e568b8099ab2439d6b182ab4aca8e8c27d7
MohamedMurshalIbrahim <mohamedtechcreation@gmail.com>
2026-07-06 11:20:26 +0530

    feat: Modified Command Lines and Added Titles in D1-S1

--- Day1 - 06July/Session1/main.c
+++ Day1 - 06July/Session1/main.c
@@ -1,6 +1,11 @@
+// C Program Training
+// Day 1(06-July), Session 1(9.10AM to 10.50AM)
+
 #include <stdio.h> //Header file for input and output functions
 
 int main() {
+    printf("C Program Training\n");
+    printf("Day 1(06-July), Session 1(9.10AM to 10.50AM)\n");
     printf("Name    : Mohamed Murshal Ibrahim T\n"); //printing the name of the student
     printf("ID      : 23UEC036\n"); //printing the ID of the student
     printf("Course  : B.E. Electronics and Communication Engineering\n"); //printing the course of the student
